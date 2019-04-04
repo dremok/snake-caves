2b9433a385f5f33c5cb934df7d50a03aa5b66af3
Max Leander <mlea@hellofresh.com>
2019-04-04 10:53:27 +0200

    Possible to save state.

--- examples/ff_model.py
+++ examples/ff_model.py
@@ -1,7 +1,27 @@
+from torch import nn
+
 from model.base_model import BaseModel
 
 
-class FeedForwardModel(BaseModel):
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+class FeedForwardClassificationModel(BaseModel):
+    def __init__(self):
+        modules = {'net': Net(784, 512, 10)}
+        super().__init__(modules)
+
     def train(self):
         pass
 
--- model/base_model.py
+++ model/base_model.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 from typing import Dict
 
+import torch
 from torch import nn
 
 
 class BaseModel(ABC):
-    def __init__(self):
-        self._model_states: Dict[str, nn.Module] = {}
+    def __init__(self, modules: Dict[str, nn.Module]):
+        self._modules: Dict[str, nn.Module] = modules
 
     @abstractmethod
     def train(self):
@@ -19,4 +20,8 @@ class BaseModel(ABC):
     def load_state(self, model_state):
         for state_key in model_state:
             state_dict = model_state[state_key]
-            self._model_states[state_key].load_state_dict(state_dict)
+            self._modules[state_key].load_state_dict(state_dict)
+
+    def save_state(self, model_state_file):
+        torch.save({module_name: module.state_dict() for module_name, module in self._modules.items()},
+                   model_state_file)
